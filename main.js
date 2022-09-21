8a9fcf7b700ec6ea68484575893f6d0baaab74a7
Gabriel Christino Cordeiro dos Santos <gabriel.christino@gmail.com>
2022-09-21 16:48:33 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-D4.set(); // LCD backlight on
-var spi = new SPI();
-spi.setup({mosi:D19 /* sda */, sck:D18 /* scl */});
-var g = require("ST7789").connect(spi, D16 /* DC */, D5 /* CE */, D23 /* RST */, function() {});
 exports.init = function (){
-  return g;
+  D4.set(); // LCD backlight on
+  var spi = new SPI();
+  spi.setup({mosi:D19 /* sda */, sck:D18 /* scl */});
+  return require("ST7789").connect(spi, D16 /* DC */, D5 /* CE */, D23 /* RST */, function() {});
 }
