36b17462746236a8c2244c0ac4dce689512a9ece
gabrielchristino <gabriel.christino@gmail.com>
2022-09-22 18:02:32 -0300

    feat screen to constructor

--- main.js
+++ main.js
@@ -1,29 +1,21 @@
 class mainChronvs {
-  constructor() {
+  constructor(tela) {
     var appsModuleList;
-    // var appsList = [{}];
+    var appsList = new Array();
+    var selectedItem = -1;
+    var currentItem = 0;
+    var tela = tela;
   }
   getApps() {
     this.appsModuleList = require("Storage").list(/\apjs/);
     return this.appsModuleList;
   }
-
   showMenu() {
-    var appsList = new Array();
     function loadApp(appName, index) {
       var app = new (require(appName))();
-      console.log(appName);
-      console.log(app.getName());
-      console.log(app.getIcon());
-      console.log('debug');
-      if (Array.isArray(appsList)) {
-        appsList.push({appName, name: app.getName(), icon: app.getIcon()});
-        console.log('passou aqui');
-      }
+      this.appsList.push({appName, name: app.getName(), icon: app.getIcon()});
     }
     this.appsModuleList.forEach(loadApp);
-
-    console.log(appsList);
   }
 
 }
