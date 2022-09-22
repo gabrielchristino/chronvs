8e03d6a1a0275411f952e68bccf64da793615cba
gabrielchristino <gabriel.christino@gmail.com>
2022-09-22 18:45:54 -0300

    core

--- chronvs.js
+++ chronvs.js
@@ -1,24 +1,24 @@
 var g = new (require("https://gabrielchristino.github.io/chronvs/screenConfig.js"))();
-var mainChronvs = new (require("https://gabrielchristino.github.io/chronvs/main.js"))();
 
 g.connect();
 g.setFont();
 var tela = g.getScreen();
+
+var mainChronvs = new (require("https://gabrielchristino.github.io/chronvs/main.js"))(tela);
+
 tela.clear();
 tela.setRotation(1);
 
 var appsName = mainChronvs.getApps();
-var app = new (require("answapjs"))();
+var app = new (require("answapjs"))(tela);
+
 setWatch(function(){
-  D4.set();
-  tela.clear();
-  //tela.drawString(appsName,0,0);
-  mainChronvs.showMenu();
-  app.onClickBtn35();
+  mainChronvs.move(-1);
+  //mainChronvs.showMenu();
+  //app.onClickBtn35();
 }, D35, { repeat: true, edge: "falling" });
 
 setWatch(function(){
-  tela.clear();
-  D4.reset();
-  app.onClickBtn0();
+  //app.onClickBtn0();
+  mainChronvs.move(1);
 }, D0, { repeat: true, edge: "falling" });
