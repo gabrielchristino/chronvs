7358baceb6930309273227f00034f09203824b19
gabrielchristino <gabriel.christino@gmail.com>
2022-09-22 16:56:03 -0300

    debug

--- main.js
+++ main.js
@@ -14,10 +14,8 @@ class mainChronvs {
       console.log(item);
       console.log(app.getName());
       console.log(app.getIcon());
-      //this.appsList.push({app: item, name: app.getName(), icon: app.getIcon()});
       console.log('');
     }
-    //console.log(this.appsList);
     this.appsModuleList.forEach(loadApp);
   }
 
