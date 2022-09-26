44d45c01b8048952dddad0d0004e71cb0974349f
gabrielchristino <gabriel.christino@gmail.com>
2022-09-26 16:18:41 -0300

    feat rename files

--- chronvs.js
+++ chronvs.js
@@ -1,29 +1,23 @@
-D4.set(); // LCD backlight on
-var spi = new SPI();
-spi.setup({mosi:D19 /* sda */, sck:D18 /* scl */});
-var tela = require("ST7789").connect(spi, D16 /* DC */, D5 /* CE */, D23 /* RST */, function() {});
+var tela = new (require("https://gabrielchristino.github.io/chronvs/libscreen"))().connect();
+var utils = new (require("libutils"))(this.tela);
 
-var mainChronvs = new (require("main"))(this.tela);
+var main = new (require("libmain"))(this.tela, this.utils);
+var app = this.main;
 
 this.tela.setRotation(1);
 this.tela.clear();
 
-var appsName = mainChronvs.getApps();
-var app = new (require("answapjs"))(this.tela);
+this.app.onInit();
 
 setWatch(function(){
-  console.log("1");
-  app.onClickBtn35();
-  //mainChronvs.move(-1);
+  this.app = this.app.onClickBtn0();
+}, D12, { repeat: true, edge: "falling", debounce:30 });
+
+setWatch(function(){
+  this.app = this.app.onClickBtn35();
 }, D35, { repeat: true, edge: "falling", debounce:30 });
 
 setWatch(function(){
-  console.log("2");
-  app.onClickBtn0();
-  //mainChronvs.move(1);
-}, D0, { repeat: true, edge: "falling", debounce:30 });
-
-/*setWatch(function(){
-  //app.onClickBtn0();
-  mainChronvs.open();
-}, D12, { repeat: true, edge: "falling", debounce:20 });*/
+  this.app = this.app.onClickBtn12();
+  this.app.onInit();
+}, D0, { repeat: true, edge: "falling", debounce:20 });
