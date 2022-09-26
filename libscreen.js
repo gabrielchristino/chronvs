25cf17fc6395c7e6d337323b62bb374f1f6ba58d
gabrielchristino <gabriel.christino@gmail.com>
2022-09-26 19:48:32 -0300

    fix lib screen

--- libscreen.js
+++ libscreen.js
@@ -3,6 +3,7 @@ class screenChronvs {
     var tela = {};
   }
   connect() {
+    D4.reset()
     var spi = new SPI();
     spi.setup({mosi:D19 /* sda */, sck:D18 /* scl */});
     this.tela = require("ST7789").connect(spi, D16 /* DC */, D5 /* CE */, D23 /* RST */, function() {});
