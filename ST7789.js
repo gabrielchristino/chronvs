4e262bdcc706dafccb5c0c69d7ad0d8beb668d1e
gabrielchristino <gabriel.christino@gmail.com>
2022-09-27 13:20:12 -0300

    debug

--- ST7789.js
+++ ST7789.js
@@ -21,7 +21,7 @@ var g = require("ST7789").connect(spi, D16, D5, D23, function() {
 */
 const LCD_WIDTH = 240;
 const LCD_HEIGHT = 135;
-const COLSTART = 40;
+const COLSTART = 60;
 const ROWSTART = 52;
 
 
