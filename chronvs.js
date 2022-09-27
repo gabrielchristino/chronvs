01c5c5ccfb9fac12e95d62cb8b220c35c30ac4ae
gabrielchristino <gabriel.christino@gmail.com>
2022-09-27 11:02:55 -0300

    new ST7789

--- chronvs.js
+++ chronvs.js
@@ -6,17 +6,44 @@ var app = this.main;
 
 this.tela.setRotation(1);
 this.tela.clear();
-this.app.onInit();
 
-setWatch(function(){
-  this.app = this.app.onClickBtn0();
-}, D12, { repeat: true, edge: "falling", debounce:30 });
+setWatch(function(e){
+  if(e.time-e.lastTime > 1) {
+    this.app = this.app.onClickBtn12();
+    this.app.onInit();
+  } else {
+    this.app = this.app.onClickBtn0();
+  }
+}, D0, { repeat: true, edge: "rising", debounce:30 });
 
-setWatch(function(){
-  this.app = this.app.onClickBtn35();
-}, D35, { repeat: true, edge: "falling", debounce:30 });
+setWatch(function(e){
+  if(e.time-e.lastTime > 1) {
+    this.app = this.app.onClickBtn21();
+    this.app.onInit();
+  } else {
+    this.app = this.app.onClickBtn35();
+  }
+}, D35, { repeat: true, edge: "rising", debounce:30 });
 
-setWatch(function(){
-  this.app = this.app.onClickBtn12();
-  this.app.onInit();
-}, D0, { repeat: true, edge: "falling", debounce:20 });
+setWatch(function(e){
+  if(e.time-e.lastTime > 1) {
+    console.log("long");
+  } else {
+    this.app = this.app.onClickBtn12();
+    this.app.onInit();
+  }
+}, D12, { repeat: true, edge: "rising", debounce:20 });
+
+setWatch(function(e){
+  if(e.time-e.lastTime > 1) {
+    console.log("long");
+  } else {
+    console.log("short");
+    this.app.onInit();
+  }
+}, D21, { repeat: true, edge: "rising", debounce:20 });
+
+setTimeout(() => {
+  this.tela.setRotation(1);
+  this.tela.clear();
+}, 100);
