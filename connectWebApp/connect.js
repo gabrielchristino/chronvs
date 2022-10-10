07d734000e465c726371e81343ce529ed1124e6d
gabrielchristino <gabriel.christino@gmail.com>
2022-10-10 16:42:38 -0300

    test manufacturer

--- connectWebApp/connect.js
+++ connectWebApp/connect.js
@@ -3,13 +3,17 @@ async function onButtonClick() {
     try {
         navigator.bluetooth.requestDevice({
             filters: [{
-              name: 'ESP32'
+              manufacturerData: [{
+                companyIdentifier: 0x0590,
+                dataPrefix: new Uint8Array([0x01, 0x02])
+              }]
             }],
             optionalServices: ['battery_service'] // Required to access service later.
           })
           .then(device => {
             // Human-readable name of the device.
             console.log(device.name);
+            console.log(device);
           
             // Attempts to connect to remote GATT Server.
             return device.gatt.connect();
