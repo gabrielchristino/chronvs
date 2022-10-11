384092170cd79a834505d6223ddbea1998412ffb
gabrielchristino <gabriel.christino@gmail.com>
2022-10-11 17:13:12 -0300

    notify by spot

--- chronvs.js
+++ chronvs.js
@@ -34,3 +34,28 @@ setTimeout(() => {
   this.tela.clear();
   this.utils.getTime();
 }, 10);
+
+var page = "<html><body>teste</body></html>";
+
+function onPageRequest(req, res) {
+  var a = url.parse(req.url, true);
+  var data = "";
+  var postData = {};
+  req.on('data', function(d) { data += d; });
+  req.on('end', function() {
+    postData = JSON.parse(data);
+  });
+  if (a.pathname == "/notify") {
+    utils.clearScreen();
+    utils.drwStr(postData.title + "\n" + postData.text ,0,0);
+  }
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(page);
+}
+
+function criarServer() {
+  require("http").createServer(onPageRequest).listen(80);
+}
+
+var wifi = require("Wifi");
+wifi.connect("tests", {password:"tests123"}, criarServer);
