9b6088c8e23cc22fbb888846daf35ccc14190c57
gabrielchristino <gabriel.christino@gmail.com>
2022-10-13 14:57:36 -0300

    http module test

--- chronvs.js
+++ chronvs.js
@@ -38,22 +38,42 @@ setTimeout(() => {
 var page = "<html><body>teste</body></html>";
 
 function onPageRequest(req, res) {
+  this.utils.stopTimeout();
   var a = url.parse(req.url, true);
   var data = "";
   var postData = {};
   req.on('data', function(d) { data += d; });
   req.on('end', function() {
     postData = JSON.parse(data);
+    if (a.pathname == "/notify" && !postData.packageName.includes("systemui")) {
+      utils.clearScreen();
+      console.log(postData);
+      utils.drwStr(postData.title + "\n" + postData.text ,0,0);
+      this.utils.startTimeout();
+    }
   });
-  if (a.pathname == "/notify") {
-    utils.clearScreen();
-    utils.drwStr(postData.title + "\n" + postData.text ,0,0);
-  }
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end(page);
 }
 
+function loadModule(moduleName, callback) {
+  console.log("https://gabrielchristino.github.io/chronvs/"+moduleName);
+  require("http").get("http://gabrielchristino.github.io/chronvs/"+moduleName, function(res) {
+    res.on('data', function(data) {
+      console.log("HTTP> "+data);
+    });
+    res.on('close', function(data) {
+      console.log("Connection closed");
+    });
+  });
+}
+
 function criarServer() {
+  console.log("conectado");
+  this.loadModule("apjsansw.js", function() {
+    //var teste = new (require("apjsansw.js"))();
+  });
+  Modules.getCached();
   require("http").createServer(onPageRequest).listen(80);
 }
 
